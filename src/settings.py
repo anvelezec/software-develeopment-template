01898a87be1705838f8d567ebf136985be6957d9
Andres Velez <anvelezec@unal.edu.co>
2022-10-11 12:00:09 -0500

    read .env file in module

--- src/settings.py
+++ src/settings.py
@@ -7,5 +7,5 @@ class Settings(BaseSettings):
     PROJECT_NAME: str
 
     class Config:
-        env_file = os.path.join(os.path.dirname(__file__), "../src/.env")
+        env_file = os.path.join(os.path.dirname(__file__), ".env")
         env_file_encoding = "utf-8"
